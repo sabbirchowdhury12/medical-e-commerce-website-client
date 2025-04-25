b79a139bc4673b4e0e918618312f0aa8d2b84df7
Sabbir <sabbirchowdhury40854@gmail.com>
2025-04-25 08:41:40 +0600

    chnages

--- redux/api/baseApi.ts
+++ redux/api/baseApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://localhost:5000/api/v1",
-    baseUrl: "https://pharmaplus-ecommerce.vercel.app/api/v1",
+    baseUrl: "http://localhost:5000/api/v1",
+    // baseUrl: "https://pharmaplus-ecommerce.vercel.app/api/v1",
   }),
   endpoints: () => ({}),
   // tagTypes: ['driver', 'vehicle', 'profile'],
